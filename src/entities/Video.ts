5007238155734066491da9e0ab640ee3d3f51c92
kravchenko-d <kravchenko4@mail.ru>
2023-10-12 06:50:27 +0300

    description styles

--- src/entities/Video.ts
+++ src/entities/Video.ts
@@ -28,18 +28,28 @@ export default class Video {
 
         const title = document.createElement('div')
         const channel = document.createElement('div')
+        const metadata = document.createElement('div')
+        metadata.classList.add('metadata')
         const views = document.createElement('div')
+        const date = document.createElement('div')
         preview.appendChild(img)
         description.appendChild(title)
         description.appendChild(channel)
-        description.appendChild(views)
-        // description.appendChild(date)
+        description.appendChild(metadata)
+        metadata.appendChild(views)
+        metadata.appendChild(date)
+        const a = document.createElement('a')
+        a.href = this.link
         title.innerText = this.title
         channel.innerText = this.channel
-        views.innerText = `${this.views} просмотров`
+        views.innerText = `${this.views} просмотров •`
+        date.innerText = this.date
 
         this.element.innerHTML=''
-        this.element.appendChild(preview)
-        this.element.appendChild(description)
+        // this.element.appendChild(preview)
+        // this.element.appendChild(description)
+        this.element.appendChild(a)
+        a.appendChild(preview)
+        a.appendChild(description)
     }
 }
