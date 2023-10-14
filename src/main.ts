cd40fea38e34c85ad2ca34000afe2ea154287b67
kravchenko-d <kravchenko4@mail.ru>
2023-10-14 08:10:10 +0300

    YouTube

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,16 @@ import YouTube from "./entities/YouTube"
 const youtube = new YouTube(document.getElementById('app'))
 
 youtube.init()
+
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const youtube_search = 'https://www.youtube.com/results?search_query=';
+
+function submitted(event) {
+    event.preventDefault();
+    const url = youtube_search + q.value;
+    const win = window.open(url, '_blank');
+    win!.focus();
+  }
+  
+  f!.addEventListener('submit', submitted);
